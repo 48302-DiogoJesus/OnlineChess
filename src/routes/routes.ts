87a6520c8d425abb2430a69340dc0bd964427840
Diogo Jesus <a48302@alunos.isel.pt>
2022-07-12 15:43:40 +0100

    A

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -21,9 +21,7 @@ app.use((req, res, next) => {
     res.set('Access-Control-Allow-Credentials', 'true')
     next()
 })
-app.use(cors({
-    origin: CONFIG.PROD ? "" : "*",
-}))
+app.use(cors())
 app.use(session({
     secret: 'chessbackend',
     resave: false,
